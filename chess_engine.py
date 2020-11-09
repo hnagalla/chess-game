53170df174221c89db1b995f7c1cc7644ff67fca
Harish Nagallapati <hnagalla@uwo.ca>
2020-11-08 20:37:58 -0500

    Added draw current state (pieces) functionality

--- chess_engine.py
+++ chess_engine.py
@@ -1,3 +1,26 @@
 #STORES ALL INFO ABOUT CURRENT STATE OF CHESS GAME
 #DETERMINES VALID MOVES FOR CURRENT STATE
 #STORES A MOVE LOG
+
+class GameState():
+    def __init__(self):
+        #2-D List (list of list of pieces on board currently)
+        self.__board_state = [
+            ['bR','bN','bB','bQ','bK','bB','bN','bR'],
+            ['bp','bp','bp','bp','bp','bp','bp','bp'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['wp','wp','wp','wp','wp','wp','wp','wp'],
+            ['wR','wN','wB','wQ','wK','wB','wN','wR'],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+
+    @property
+    def board_state(self):
+        return self.__board_state
+
+
+    
